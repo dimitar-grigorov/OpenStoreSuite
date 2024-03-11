2e30382e57cb0537fd2a60937a42e20b7a1ed912
Dimitar Grigorov <mitak.backtrack@gmail.com>
2024-03-11 23:05:51 +0200

    Added Initial files.

---
+++ OpenStoreSuite.dpr
@@ -0,0 +1,13 @@
+program OpenStoreSuite;
+
+uses
+  Forms,
+  MainFUnit in 'MainFUnit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
